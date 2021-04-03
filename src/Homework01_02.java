fec8a7a75a1f27ef1f9cab5decac00b5c91399a0
Herminia Woodwright <hwoodwri@si-d3.kzn.21-school.ru>
2021-04-03 12:37:24 +0300

    01_02

--- src/Homework01_02.java
+++ src/Homework01_02.java
@@ -1,38 +1,10 @@
 import java.util.Scanner;
 
 public class Homework01_02 {
-    public static void main (String[] args) {
-
-//        Scanner in = new Scanner(System.in);
-//        String param = in.next();
-//        String numbArr = in.next();
-//        int numb = Integer.parseInt(numbArr);
-//        System.out.println(numb);
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String numbers = scanner.next();
+        int k = scanner.nextInt();
+        System.out.println(numbers.charAt(k - 1));
     }
 }
-
-//    Класс StringTokenizer разбивает строку на токены с указанным разделителем.
-
-//    StringTokenizer tokenizer = new StringTokenizer("Кот Васька", " ");
-//while (tokenizer.hasMoreElements()){
-//        System.out.println(tokenizer.countTokens() + " words remaining to print");
-//        System.out.println(tokenizer.nextToken());
-
-
-
-
-
-//    public String[] split (String regularExpression) и другие перегруженные версии
-//    Разбивает строку на массив из слов. Например, есть строка Васька Рыжик Мурзик Барсик и мы хотим получить массив имён котов:
-//
-//
-//        String catNames = "Васька Рыжик Мурзик Барсик";
-//        String aCats[] = catNames.split(" ");  // по пробелу
-//        Получим:
-//
-//        aCats[0] = Васька
-//        aCats[1] = Рыжик
-//        aCats[2] = Мурзик
-//        aCats[3] = Барсик
-
-//12,32,3,50,94,659,84,3
