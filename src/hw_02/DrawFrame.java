654faab69cc0b72f32d4503d8d2ceb661a92cd92
Herminia Woodwright <hwoodwri@si-d3.kzn.21-school.ru>
2021-04-11 21:05:16 +0300

    hw02

---
+++ src/hw_02/DrawFrame.java
@@ -0,0 +1,60 @@
+package hw_02;
+
+import java.util.Scanner;
+
+public class DrawFrame {
+
+    // Написать программу, выполнящую рисование рамки вокруг текстовой строки.
+    // Программа олжна принимать на вход размеры рамки (длина, ширина) и саму строку.
+    // Текстовая строка должна быть отцентрована как по горизонтали, так и по вертикали.
+    // В случае, если длина строки не позволяет вписать строку в рамку заданного размера,
+    // программа должна вместо рамки выводить сообщение "Ошибка".
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int height = in.nextInt();
+        int width = in.nextInt();
+        String str = in.next();
+        int strLen = str.length();
+        int widthParity = (width - 2 - strLen) % 2;
+        int heightParity = (height - 3) % 2;
+
+        if (width < strLen + 2 || height < 3) {
+            System.out.println("Error");
+            return;
+        }
+
+        for (int i = 0; i < height; i++) {
+
+            if (i == 0 || i == height - 1) {
+                for (int j = 0; j < width; j++) {
+                    System.out.print("*");
+                }
+                System.out.println();
+            }
+
+            else if (i == height / 2) {
+                System.out.print("*");
+                if (widthParity != 0) {
+                    System.out.print(" ");
+                }
+                for (int k = 0; k < (width - 2 - strLen) / 2; k++) {
+                    System.out.print(" ");
+                }
+                System.out.print(str);
+                for (int k = 0; k < (width - 2 - strLen) / 2; k++) {
+                    System.out.print(" ");
+                }
+                System.out.println("*");
+            }
+
+            else {
+                System.out.print("*");
+                for (int k = 0; k < width - 2; k++) {
+                    System.out.print(" ");
+                }
+                System.out.println("*");
+            }
+        }
+    }
+}
